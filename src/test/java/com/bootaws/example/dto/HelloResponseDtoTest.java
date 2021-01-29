f6302ee6b56f3c9dceea0d55828828776be481bf
pbs <indongcha@naver.com>
2021-01-29 15:10:23 +0900

    jUnit 실습

---
+++ src/test/java/com/bootaws/example/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.bootaws.example.dto;
+
+import com.bootaws.example.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        String name = "TEST";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+}
